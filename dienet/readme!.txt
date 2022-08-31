7e21783c8eacb9196f334a3580c11d4017c82375
Die Nock <108774168+dienock0@users.noreply.github.com>
2022-08-31 19:13:32 +0300

    Add files via upload

---
+++ dienet/readme!.txt
@@ -0,0 +1,6 @@
+Turkish
+
+Eğer Arch linux kullanıyorsanız ve sisteminizde "Nmap" ve "nslookup" toolları yüklü değil ise ilk olarak
+tools.py dosyasını çalıştırınız. İyi çalışmalar !
+
+Ayrıca kullanmadan önce requirements dosyasındaki gerekli kütüphaneleri indirmenizi öneriririm !
---
+++ dienet/tools.py
@@ -0,0 +1,24 @@
+import subprocess
+import time
+import colorama
+
+from colorama import Fore, Back, Style
+
+
+ilk_yazi = "Gerekli Toollar Indirilmeye başlıyor ..."
+
+print(Fore.LIGHTRED_EX+ilk_yazi)
+
+time.sleep(2)
+
+subprocess.call(["sudo","pacman","-S","nmap"])
+
+time.sleep(1)
+
+subprocess.call(["sudo","pacman","-S","dnsutils"])
+
+son_yazi = "Indirme başariyla tamamlandi, Terminali kapatabilirsiniz ! "
+
+print(Fore.LIGHTCYAN_EX+son_yazi)
+
+
